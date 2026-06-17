e12336a588d604d4135ca18c4b22920a41df338e
InzamanCareem <blankfindertechnotommy@gmail.com>
2026-06-17 22:56:16 +0530

    Added training manager

---
+++ run_manager.py
@@ -0,0 +1,40 @@
+from PyQt6.QtCore import QThreadPool
+
+from train_worker import TrainWorker
+from train_model import *
+
+
+class RunManager:
+    def __init__(self):
+        self.threadpool = QThreadPool()
+
+        self.X = None
+        self.y = None
+
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
+
+        self.model = None
+
+        # load the dataset
+        # load the model
+        # keep the worker light
+
+    def load_dataset(self, dataset_type, samples, features):
+        self.X, self.y = get_data(dataset_type, samples, features)
+
+    def start(self):
+        self.X_train, self.y_train, self.X_test, self.y_test = make_train_test(self.X, self.y)
+        self.X_train, self.y_train, self.X_test, self.y_test = preprocess(self.X_train, self.y_train, self.X_test,
+                                                                          self.y_test)
+
+        self.model = make_model()
+
+        train_worker = TrainWorker()  # need the dataset and the model
+
+        # train_worker.signals.run_config.connect(self.plot_panel.plot_curve)
+        # train_worker.signals.run_config.connect(self.progress_panel.save_run)
+
+        self.threadpool.start(train_worker)
